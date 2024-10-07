7a7cff7ecfb83d1631f3ce17fcfd2b21f4f7996c
Caleb Fonyuy <fonyuy-asheri.caleb@inria.fr>
2024-10-07 09:50:08 +0200

    initial commit

---
+++ profile.py
@@ -0,0 +1,42 @@
+# Import the Portal object.
+import geni.portal as portal
+# Import the ProtoGENI library.
+import geni.rspec.pg as pg
+# Import the Emulab specific extensions.
+import geni.rspec.emulab as emulab
+
+# Create a portal context.
+pc = portal.Context()
+
+# Create a Request object to start building the RSpec.
+request = pc.makeRequestRSpec()
+
+osImage = 'urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU22-64-STD'
+
+clientTypes = ['r320', 'r320', 'c6220']
+
+ip_count = 2
+ifaces = []
+
+for c_type in clientTypes:
+    nfsClient = request.RawPC(c_type + "_node-"+str(ip_count))
+    nfsClient.disk_image = osImage
+    nfsClient.hardware_type = c_type
+    nfsClient.routable_control_ip = True
+    c_iface = nfsClient.addInterface('interface-'+str(ip_count), pg.IPv4Address('192.168.6.'+str(ip_count),'255.255.255.0'))
+    ifaces.append(c_iface)
+    ip_count = ip_count + 1
+
+
+# The NFS network. All these options are required.
+nfsLan = request.LAN(nfsLanName)
+# Must provide a bandwidth. BW is in Kbps
+nfsLan.bandwidth         = 100000
+nfsLan.best_effort       = True
+nfsLan.vlan_tagging      = True
+nfsLan.link_multiplexing = True
+for iface in ifaces:
+    nfsLan.addInterface(iface)
+
+# Print the RSpec to the enclosing page.
+pc.printRequestRSpec(request)
